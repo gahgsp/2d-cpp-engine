132e43cc6cfd3eb538f1a68d5dc42001dabbe97f
gahgsp <kelvinbianchi@gmail.com>
2020-06-25 21:05:55 -0300

    Fixed bug with the camera clamping

--- src/Game.cpp
+++ src/Game.cpp
@@ -302,15 +302,15 @@ void Game::Render() {
 }
 
 void Game::HandleCameraMovement() {
-   TransformComponent* mainPlayerTransform = player->GetComponent<TransformComponent>();
-    camera.x = mainPlayerTransform->position.x - (WINDOW_WIDTH / 2);
-    camera.y = mainPlayerTransform->position.y - (WINDOW_HEIGHT / 2);
+   TransformComponent* playerTransform = player->GetComponent<TransformComponent>();
+    camera.x = playerTransform->position.x - (WINDOW_WIDTH / 2);
+    camera.y = playerTransform->position.y - (WINDOW_HEIGHT / 2);
 
     // Clamping the values of the camera.
     camera.x = camera.x < 0 ? 0 : camera.x;
     camera.y = camera.y < 0 ? 0 : camera.y;
-    camera.x > camera.w ? camera.w : camera.x;
-    camera.y > camera.h ? camera.h : camera.y;
+    camera.x = camera.x > camera.w ? camera.w : camera.x;
+    camera.y = camera.y > camera.h ? camera.h : camera.y;
 }
 
 void Game::CheckCollisions() {
